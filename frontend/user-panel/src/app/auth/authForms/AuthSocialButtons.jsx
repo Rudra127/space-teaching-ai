5893f70e4a75cfa5b92dd7acb79172cf494e1e58
Rudra <109367898+Rudra127@users.noreply.github.com>
2024-10-06 20:41:24 +0530

    Update AuthSocialButtons.jsx

--- frontend/user-panel/src/app/auth/authForms/AuthSocialButtons.jsx
+++ frontend/user-panel/src/app/auth/authForms/AuthSocialButtons.jsx
@@ -31,7 +31,7 @@ const AuthSocialButtons = ({ title, callbackUrl }) => (
         >
           {title}{' '}
         </Box>{' '}
-        Google
+        Google.
       </CustomSocialButton>
     </Stack>
   </>
